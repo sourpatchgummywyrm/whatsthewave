eae901e2bbda883c09b9350b3620640303c5f614
Damola Olugboji <damolaolugboji@gmail.com>
2019-10-19 11:46:20 -0400

    yer

--- wtw/lib/main.dart
+++ wtw/lib/main.dart
@@ -1,18 +1,4 @@
-/*import 'package:flutter/material.dart';
-import './newuser/Login.dart';
-import './main/home[1].dart';
 
-need to do: 
-- add photoshop picture of WTW Logo (tentative subject to change)
-- figure out animations from different screens
-- database shit
-- need login screen (will skip for now just to get base product functionality out)
-- increase width of login & signup button
-- maybe decrease height of button
-- new user.dart animations. Slowly fade up
-
-- add animatino 
-*/
 import 'package:flutter/material.dart';
 import 'package:wtw/global.dart';
 import './newuser/Start.dart';
