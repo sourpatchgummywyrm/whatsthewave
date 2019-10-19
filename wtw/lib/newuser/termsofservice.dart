5c82bac87a3c260ba24292270229545fbf0da8d5
stephenjuan <stephenjuan11@gmail.com>
2019-10-19 17:59:24 -0400

    flkjefkjsef ored, and an empty message aborts the commit.

--- wtw/lib/newuser/termsofservice.dart
+++ wtw/lib/newuser/termsofservice.dart
@@ -4,14 +4,7 @@ import 'package:flutter/material.dart';
 class Terms extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      backgroundColor: paleRedColor,
-      body: new Center(
-          child: new Text(
-        "Terms of Service",
-        style: reggie3,
-      )),
-    );
+    return FirstRoute();
   }
 }
 
