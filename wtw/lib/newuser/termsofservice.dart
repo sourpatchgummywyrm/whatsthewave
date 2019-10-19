d445526b9a0721096f20add4032780e2b93ca661
stephenjuan <stephenjuan11@gmail.com>
2019-10-19 17:54:51 -0400

    kfjdnsk

--- wtw/lib/newuser/termsofservice.dart
+++ wtw/lib/newuser/termsofservice.dart
@@ -8,9 +8,49 @@ class Terms extends StatelessWidget {
       backgroundColor: paleRedColor,
       body: new Center(
           child: new Text(
-        "Terms of Service kjnskjnc...",
+        "Terms of Service",
         style: reggie3,
       )),
+    );
+  }
+}
+
+class FirstRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: RaisedButton(
+          child: Text(
+            'Terms of Service', 
+            style: reggie3),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SecondRoute()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class SecondRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Terms of Service"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('Return to Login'),
+        ),
+      ),
     );
   }
 }
